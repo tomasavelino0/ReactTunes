7d04e263e767e879ca28829b7aa32fcc7ca3124a
tomas <tomasdc2016@gmail.com>
2022-08-02 14:28:45 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,53 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  state = {
+    btnSearchDisable: true,
+    search: '',
+  }
+
+  onInputChange = ({ target }) => {
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    this.setState({
+      [target.name]: value,
+    }, () => {
+      this.btnEnable();
+    });
+  };
+
+  btnEnable = () => {
+    const minCaracters = 2;
+    const { search } = this.state;
+    this.setState({
+      btnSearchDisable: search.length < minCaracters,
+    });
+  }
+
   render() {
+    const { btnSearchDisable, search } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <div className="input-search">
+          <form action="">
+            <label htmlFor="search">
+              <input
+                onChange={ this.onInputChange }
+                value={ search }
+                type="text"
+                name="search"
+                data-testid="search-artist-input"
+              />
+            </label>
+            <button
+              disabled={ btnSearchDisable }
+              type="button"
+              data-testid="search-artist-button"
+            >
+              Pesquisar
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
